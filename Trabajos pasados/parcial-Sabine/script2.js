196f4cdb896c61094d862a820a677d056e4c7846
iMuffins <byaken@protonmail.com>
2021-05-24 10:14:00 -0500

    commit con los cambios de interfaz 2 de Sabine, se borra interfaz 1 descartada, cambio de imágenes y colores, se acomodan los divs para mantener alineamientos, falta acomodar botón del header y footer que queda debajo de contenido

---
+++ Trabajos pasados/parcial-Sabine/script2.js
@@ -0,0 +1,85 @@
+var num1;
+var result;
+
+function suma() {
+    num1 = document.getElementById("numBox1").value*1;
+    console.log(num1);
+    result= ++num1;
+    document.getElementById("numBox1").value = result;
+}
+function resta() {
+    num1 = document.getElementById("numBox1").value*1;
+    console.log(num1);
+    result= --num1;
+    document.getElementById("numBox1").value = result;
+}
+
+
+function suma2() {
+    num1 = document.getElementById("numBox2").value*1;
+    console.log(num1);
+    result= ++num1;
+    document.getElementById("numBox2").value = result;
+}
+function resta2() {
+    num1 = document.getElementById("numBox2").value*1;
+    console.log(num1);
+    result= --num1;
+    document.getElementById("numBox2").value = result;
+}
+
+
+function suma3() {
+    num1 = document.getElementById("numBox3").value*1;
+    console.log(num1);
+    result= ++num1;
+    document.getElementById("numBox3").value = result;
+}
+function resta3() {
+    num1 = document.getElementById("numBox3").value*1;
+    console.log(num1);
+    result= --num1;
+    document.getElementById("numBox3").value = result;
+}
+
+
+function suma4() {
+    num1 = document.getElementById("numBox4").value*1;
+    console.log(num1);
+    result= ++num1;
+    document.getElementById("numBox4").value = result;
+}
+function resta4() {
+    num1 = document.getElementById("numBox4").value*1;
+    console.log(num1);
+    result= --num1;
+    document.getElementById("numBox4").value = result;
+}
+
+
+function suma5() {
+    num1 = document.getElementById("numBox5").value*1;
+    console.log(num1);
+    result= ++num1;
+    document.getElementById("numBox5").value = result;
+}
+function resta5() {
+    num1 = document.getElementById("numBox5").value*1;
+    console.log(num1);
+    result= --num1;
+    document.getElementById("numBox5").value = result;
+}
+
+
+function suma6() {
+    num1 = document.getElementById("numBox6").value*1;
+    console.log(num1);
+    result= ++num1;
+    document.getElementById("numBox6").value = result;
+}
+function resta6() {
+    num1 = document.getElementById("numBox6").value*1;
+    console.log(num1);
+    result= --num1;
+    document.getElementById("numBox6").value = result;
+}
