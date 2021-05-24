6e3606d944e4497b92126c20af81cf9bb89975cc
Miguel Rojas <miguel_ang.rojas@uao.edu.co>
2021-05-24 16:25:59 -0500

    final

--- Trabajos pasados/parcial-Sabine/script2.js
+++ Trabajos pasados/parcial-Sabine/script2.js
@@ -83,3 +83,27 @@ function resta6() {
     result= --num1;
     document.getElementById("numBox6").value = result;
 }
+
+function onClickCalculator (){
+
+    this.numero1=parseInt(document.getElementById("numBox1").value);
+    this.numero2=parseInt(document.getElementById("numBox2").value);
+    this.numero3=parseInt(document.getElementById("numBox3").value);
+    this.numero4=parseInt(document.getElementById("numBox4").value);
+    this.numero5=parseInt(document.getElementById("numBox5").value);
+    this.numero6=parseInt(document.getElementById("numBox6").value);
+
+    Calcular()
+}
+
+function Calcular (){
+    //resultadoC = operacion;
+    //return resultadoC;
+    var result="";
+    result = (this.numero1 + this.numero2+ this.numero3+ this.numero4+ this.numero5+ this.numero6)*1000;
+    document.getElementById("result").innerHTML= result;
+    return result;
+
+//document.getElementById("result").innerHTML='< class="input resultadoI"> ' + result +'</div>'; 
+    
+}
