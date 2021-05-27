8ed581b8ff10e015b2814528015ae13c91a1128a
Miguel Rojas <miguel_ang.rojas@uao.edu.co>
2021-05-26 20:46:38 -0500

    Header y Footer terminados

--- Proyecto con react/proyectofinalweb/src/App.js
+++ Proyecto con react/proyectofinalweb/src/App.js
@@ -1,13 +1,14 @@
 
 import './App.css';
 import Header from "../src/components/header/header.js"
+import Footer from "../src/components/footer/footer.js"
 
 function App() {
   return (
     <div className="App">
 
      <Header></Header>
-
+     <Footer></Footer>
     </div>
   );
 }
--- Proyecto con react/proyectofinalweb/src/components/footer/footer.js
+++ Proyecto con react/proyectofinalweb/src/components/footer/footer.js
@@ -1,15 +1,117 @@
 
 import React from 'react';
-import "./footer.css"
+import "./footer.css";
+import Intagram from "../../img/instagram.png"
+import Youtube from "../../img/002-youtube.png"
+import Facebook from "../../img/045-facebook.png"
 
 
 
 
 const footer = () => (
 
-    <div className="o-">
+    <div class="footer">
 
-    </div>
+<footer class="page-footer font-small indigo">
+<div class="container text-center text-md-left">
+
+<div class="row">
+
+  <div class="col-md-3 mx-auto">
+
+   
+    <h5 class="font-weight text-uppercase mt-3 mb-4">Categoría</h5>
+
+    <ul class="list-unstyled">
+      <li>
+        <a href="#!">Supermercado</a>
+      </li>
+      <li>
+        <a href="#!">Ofertas</a>
+      </li>
+      <li>
+        <a href="#!">Productos</a>
+      </li>
+    </ul>
+
+  </div>
+  
+
+ 
+
+ 
+  <div class="col-md-3 mx-auto">
+
+ 
+    <h5 class="font-weight text-uppercase mt-3 mb-4">Información de interés</h5>
+
+    <ul class="list-unstyled">
+      <li>
+        <a href="#!">Términos y Condiciones</a>
+      </li>
+      <li>
+        <a href="#!">Contáctenos</a>
+      </li>
+      <li>
+        <a href="#!">Quienes somos</a>
+      </li>
+    </ul>
+
+  </div>
+ 
+
+
+    <div class="col-md-3 mx-center">
+
+    
+    <h5 class="font-weight text-uppercase mt-3 mb-4">Síguenos</h5>
+
+    <ul class="list-unstyled">
+            
+        <ul  class="o-list-icons">
+
+          <li class="o-list-icon-item">
+              <a href="https://www.youtube.com/channel/UCBjKZq1V936v6GdpTW053sw" target="_blank">
+                  <img class="icon" src={Youtube} alt="icono de Youtube"/> 
+              </a>
+
+          </li>
+              <li class="o-list-icon-item">
+              <a href="https://www.facebook.com/supermercadoslagrancolombia/">
+                  <img class="icon" src={Facebook}alt="icono de Facebook" target="_blank"/>
+              </a>
+          </li>
+
+          <li class="o-list-icon-item">
+              <a href="https://www.instagram.com/superlagrancolombia/">
+              <img class="icon" src={Intagram} alt="icono de instagram" target="_blank"/> 
+              </a>
+          </li>
+
+          </ul>
+
+          </ul>
+
+  </div>
+    
+
+</div>
+
+
+</div>
+
+
+
+<div class="footer-copyright text-center py-3">© 2020 Copyright: Minticris
+
+</div>
+
+
+</footer>
+
+
+
+</div>
 
 );
 
