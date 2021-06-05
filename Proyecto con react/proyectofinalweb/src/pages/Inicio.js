0d5042929f517c0f63ef291e646b5b17e8cf92c2
Miguel Rojas <miguel_ang.rojas@uao.edu.co>
2021-06-05 14:58:16 -0500

    detallitos

--- Proyecto con react/proyectofinalweb/src/pages/Inicio.js
+++ Proyecto con react/proyectofinalweb/src/pages/Inicio.js
@@ -3,6 +3,8 @@ import React from "react"
 import Header from "../components/header/header.js"
 import Footer from "../components/footer/footer.js"
 import Navlinks from "../components/links/nav-links"
+import Pruebas from "../components/prueba/prueba"
+
 
 import Banner from "../components/banner/banner"
 import NProductos from "../components/NuestrosProductos/NuestrosProductos"
@@ -18,6 +20,7 @@ function inicio() {
      <Navlinks></Navlinks>
      <Banner></Banner>
      <NProductos  title={"NUESTROS PRODUCTOS"}></NProductos>
+     <Pruebas></Pruebas>
      <Footer></Footer>
 
     </div> 
